7352585ccf966729e2111291a20531dc3eacdeb2
NotoriousWhiteHat <notoriouswhitehat@gmail.com>
2026-02-03 21:08:08 -0500

    Update ScrollingBackground.tsx file with new content

--- src/components/ScrollingBackground.tsx
+++ src/components/ScrollingBackground.tsx
@@ -1,8 +1,8 @@
 const ScrollingBackground = () => {
   return (
-    <div className="absolute inset-0 pointer-events-none overflow-hidden">
-      <div className="absolute inset-0 transform -rotate-12 scale-125">
-        {[...Array(40)].map((_, i) => (
+    <div className="fixed inset-0 pointer-events-none overflow-hidden z-0">
+      <div className="absolute inset-0 transform -rotate-12 scale-150">
+        {[...Array(60)].map((_, i) => (
           <div
             key={i}
             className={`flex whitespace-nowrap text-4xl font-black tracking-wider select-none ${
@@ -14,7 +14,7 @@ const ScrollingBackground = () => {
               marginBottom: '20px',
             }}
           >
-            {[...Array(60)].map((_, j) => (
+            {[...Array(80)].map((_, j) => (
               <span key={j} className="px-3">NOTORIOUS</span>
             ))}
           </div>
